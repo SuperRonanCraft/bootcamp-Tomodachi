2c1d8231b0e61d14b2f75c576c83a55ae8094bf5
Lovertab <lovertabrown@gmail.com>
2024-05-13 12:25:03 -0400

    added the token and verifying it

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -3,3 +3,34 @@ const jwt = require('jsonwebtoken');
 
 const secret = 'itsasecret';
 const expiration = '7d';
+
+module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user :( ', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+    if (!token) {
+      return req;
+    }
+    try {
+      const { data } = jwt.verify(token, secret, { expiresIn: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+  signToken: function ({ firstName, email, _id }) {
+    const payload = { firstName, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
