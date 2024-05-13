602a871a3d0424e504015816b7206c592fd7cf90
Lovertab <lovertabrown@gmail.com>
2024-05-13 18:35:58 -0400

    added howtoplay content to file

---
+++ client/src/components/landing/howtoplay.jsx
@@ -0,0 +1,15 @@
+const HowToPlay = () => {
+  return (
+    <>
+      <h2>How to Play</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+        delectus distinctio aperiam perferendis, accusantium suscipit neque quis
+        labore dicta? Voluptatem aut soluta magni, totam architecto repudiandae
+        consequuntur facilis provident. Corporis?
+      </p>
+    </>
+  );
+};
+
+export default HowToPlay;
