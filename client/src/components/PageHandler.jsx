f79d44e7e20e64bf55a9b8075c30b004efa4c104
Xydra <theminecraftgurru@gmail.com>
2024-05-13 12:21:52 -0400

    Changed our pageHandler to act as our Nav

--- client/src/components/PageHandler.jsx
+++ client/src/components/PageHandler.jsx
@@ -1,29 +1,26 @@
-import { useState } from 'react';
-import Home from '../pages/Home';
-import Profile from '../pages/Profile';
-import Leaderboard from '../pages/Leaderboard';
-import Game from '../pages/Game';
+import { Link } from 'react-router-dom';
 
 export default function PageHandler() {
-  const [currentPage, setCurrentPage] = useState('Home');
-
-  const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
-    }
-    if (currentPage === 'Profile') {
-      return <Profile />;
-    }
-    if (currentPage === 'Leaderboard') {
-      return <Leaderboard />;
-    } else return <Game />;
-  };
-
-  const handlePageChange = (page) => setCurrentPage(page);
-
   return (
     <div>
-      <main>{renderPage()}</main>
+      <main>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/leaderboard">Leaderboard</Link>
+            </li>
+            <li>
+              <Link to="/game">Game</Link>
+            </li>
+          </ul>
+        </nav>
+      </main>
     </div>
   );
 }
