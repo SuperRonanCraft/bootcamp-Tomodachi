eb51e51fc60c45e4375d0926d3249bc5e691778b
Hugo Hsi <hhsi@hugodehsign.com>
2024-05-13 18:43:48 -0400

    Action buttons use new ActionButton component

--- client/src/components/game/action/Play.jsx
+++ client/src/components/game/action/Play.jsx
@@ -1,15 +1,13 @@
-import { Button } from '@/components/ui/button';
+import ActionButton from './ActionButton';
+import { Gamepad2 } from 'lucide-react';
 
 export default function Play() {
   // call the game logic hook here and add onclick to buttons (for alain)
   return (
-    <Button
-      size="sm"
-      onClick={() => {
-        console.log('add the game logic function here');
-      }}
-    >
-      Play
-    </Button>
+    <ActionButton
+      onClick={() => console.log('test')}
+      text="Play"
+      icon={<Gamepad2 />}
+    />
   );
 }
