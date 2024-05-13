fb1fcdb9c5bea53baae184c64e1866ccf53a049e
Xydra <theminecraftgurru@gmail.com>
2024-05-13 11:16:42 -0400

    Imported pages to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import Home from './pages/Home.jsx';
+import Profile from './pages/Profile.jsx';
+import Game from './pages/Game.jsx';
+import Leaderboard from './pages/Leaderboard.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
