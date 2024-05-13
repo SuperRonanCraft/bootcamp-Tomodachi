2f371ffc98fec12764fa2bc5299b2b557759ee3e
Xydra <theminecraftgurru@gmail.com>
2024-05-13 16:34:48 -0400

    Undid a change

---
+++ client/src/components/login.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import auth from '../../../server/utils/auth';
+import React from 'react';
+// Add login mutation here
+
+const LoginForm = () => {
+  const [formState, setFormState] = useState(0);
+
+  // LOGIN_USER Mututation for logging in and checking for errors
+};
+
+export default LoginForm;
