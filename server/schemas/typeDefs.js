dab9792d3cf6a5bf08ebac176265d96825f8529e
Lovertab <lovertabrown@gmail.com>
2024-05-13 13:23:04 -0400

    fixed name to username and added id to updateuser

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,7 +9,13 @@ type Auth{
     token:ID
     user:User
 }
-
-
+type Query{
+    user: User
+}
+type Mutations{
+    addUser(username: String!, email: String!, password: String!): Auth
+    updateUser(username: String, _id:ID): User
+    login(username: String!, password: String!): Auth
+}
 `;
 module.exports = typeDefs;
