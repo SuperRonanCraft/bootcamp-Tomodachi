07e1e2fd095bc49655eb8258fe9f959cfe791d7d
Nora Shala <norashala00@gmail.com>
2024-05-13 11:41:53 -0400

    importing required modules

--- server/server.js
+++ server/server.js
@@ -1,15 +1,2 @@
 // Import required modules
-const express = require('express');
 
-// Set up server configuration (PORT)
-const PORT = process.env.PORT || 3000;
-
-// Create an Express application
-const app = express();
-
-app.use(express.static('public'));
-
-// Set up server setup (app.listen)
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
