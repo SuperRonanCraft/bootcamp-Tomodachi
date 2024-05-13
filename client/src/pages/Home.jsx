9a7aea64ce19b7da12122aafe2dc8abb650c9b27
Lovertab <lovertabrown@gmail.com>
2024-05-13 18:36:11 -0400

    added home file and its content for that page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,15 @@
-export default function Leaderboard() {
-  return <div>This is the Home Page</div>;
-}
+import About from '../components/landing/about';
+import HowToPlay from '../components/landing/howtoplay';
+import Nav from '../components/landing/nav';
+const Home = () => {
+  return (
+    <div className="container">
+      <h1>Welcome to Your Tomodachi</h1>
+      <About />
+      <HowToPlay />
+      <Nav />
+    </div>
+  );
+};
+
+export default Home;
