b8446565a56f25f167310873820a9400d3428495
Xydra <theminecraftgurru@gmail.com>
2024-05-13 11:40:45 -0400

    changed the root render

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,8 +7,4 @@ import Profile from './pages/Profile.jsx';
 import Game from './pages/Game.jsx';
 import Leaderboard from './pages/Leaderboard.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
