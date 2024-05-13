c596631de034f7738a70c049e7e9e3248eb2fdff
Lovertab <lovertabrown@gmail.com>
2024-05-13 19:07:33 -0400

    added more info in about and how to play

--- client/src/components/landing/howtoplay.jsx
+++ client/src/components/landing/howtoplay.jsx
@@ -3,10 +3,10 @@ const HowToPlay = () => {
     <>
       <h2>How to Play</h2>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
-        delectus distinctio aperiam perferendis, accusantium suscipit neque quis
-        labore dicta? Voluptatem aut soluta magni, totam architecto repudiandae
-        consequuntur facilis provident. Corporis?
+        1. feed
+        2.sleep
+        3. every x amount of time your food, enegry and lseep with deplete
+        4. ...
       </p>
     </>
   );
