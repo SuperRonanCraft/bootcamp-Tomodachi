f65f926a59f1010e4d26eccf88a27b17b349edc3
Nora Shala <norashala00@gmail.com>
2024-05-13 11:40:19 -0400

    branch created

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.static('public'));
 
-// Set up server setup (app.listen
+// Set up server setup (app.listen)
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
