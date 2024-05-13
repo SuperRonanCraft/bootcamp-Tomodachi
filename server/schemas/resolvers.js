61d2c1b0066fe68ec9f4cb0c9cc1f6aeb3f8a7e0
Lovertab <lovertabrown@gmail.com>
2024-05-13 14:31:38 -0400

    added login and export resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,25 +4,43 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 const resolvers = {
   Query: {
     users: async () => {
-      return await User.findById();
+      return await User.find();
     },
   },
-    Mutation: {
-      
-    addUser: async (_, args) => {
-      const user = await User.create(args);
+  Mutation: {
+    login: async (_, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('User not found');
+      }
+
+      const correctPassword = await user.isCorrectPassword(password);
+
+      if (!correctPassword) {
+        throw new AuthenticationError('Incorrect password');
+      }
+
       const token = signToken(user);
 
       return { token, user };
     },
-    updateUser: async (_, args, context) => {
-      if (context.user) {
-        return await User.findByIdAndUpdate(context.user._id, args, {
-          new: true,
-        });
-      }
+  },
+  addUser: async (_, args) => {
+    const user = await User.create(args);
+    const token = signToken(user);
 
-      throw new AuthenticationError('User not authenticated');
-    },
+    return { token, user };
+  },
+  updateUser: async (_, args, context) => {
+    if (context.user) {
+      return await User.findByIdAndUpdate(context.user._id, args, {
+        new: true,
+      });
+    }
+
+    throw new AuthenticationError('User not authenticated');
   },
 };
+
+module.exports = resolvers;
