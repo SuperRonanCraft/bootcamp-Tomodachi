d6fc7e1cd7bf96d2f5153b05b7eabc5abc5b3896
Lovertab <lovertabrown@gmail.com>
2024-05-13 18:35:42 -0400

    added about file for about content

---
+++ client/src/components/landing/about.jsx
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <>
+      <h2>About Tomodachi</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque sapiente
+        aliquam fugit perspiciatis nemo? Cupiditate, iste deserunt quisquam,
+        alias laudantium iure dolore voluptatibus, illo exercitationem veniam
+        corporis dolores facilis repellendus.
+      </p>
+    </>
+  );
+};
+
+export default About;
