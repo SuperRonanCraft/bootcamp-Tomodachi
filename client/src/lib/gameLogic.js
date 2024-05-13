d8b68dba7de4c23fa37e9514d2906661966ba4d0
RonanCraft <SuperRonanCraft@gmail.com>
2024-05-13 12:53:27 -0400

    warning and random

--- client/src/lib/gameLogic.js
+++ client/src/lib/gameLogic.js
@@ -1,11 +1,29 @@
-import Pet from "./Pet";
+import Pet from './Pet';
 
-const pet = new Pet("Alfonso", 20, 20, 60);
+const pet = new Pet('Alfonso', 20, 20, 60);
 
-export function getEnergy() {
-    return pet.energy;
+export function play() {
+  return executeAction(pet.POSSIBILITIES.play, 'play');
 }
 
-export function getFood() {
-    return pet.food;
+export function feed() {
+  return executeAction(pet.POSSIBILITIES.feed, 'feed');
+}
+
+export function sleep() {
+  return executeAction(pet.POSSIBILITIES.sleep, 'sleep');
+}
+
+// HELPER FUNCTIONS
+this.message;
+
+function warning(message) {
+  console.log(message);
+}
+
+function getRandom(array) {
+  //Takes an array
+  //Returns a random item from that array
+  var index = Math.floor(Math.random() * array.length);
+  return array[index];
 }
