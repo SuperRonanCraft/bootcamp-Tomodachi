d5a5f8c2b1b941ef927d56bb72154d7d311c3baa
Xydra <theminecraftgurru@gmail.com>
2024-05-13 14:07:19 -0400

    Imported apollo client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,20 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
 import './index.css';
 import { Outlet } from 'react-router-dom';
 import PageHandler from './components/PageHandler';
 
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <PageHandler />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
