b20364fdc20811839ae66e73e9e31c0be7060aa5
Nora Shala <norashala00@gmail.com>
2024-05-14 11:02:21 -0400

    login pg error message

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,4 +1,5 @@
 function validateForm() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
+    var errorMessage = document.getElementById('error-message');
 }
