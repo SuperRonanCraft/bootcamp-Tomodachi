38183108c271a3c96c38d00676ae3af82b4647ea
Nora Shala <norashala00@gmail.com>
2024-05-14 11:08:43 -0400

    comment validation

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,3 +1,4 @@
+// Function to validate the login form
 function validateForm() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
