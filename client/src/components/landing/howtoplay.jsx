d33c9f1e0a177a9633d2986320f94edce4c26a7a
Lovertab <lovertabrown@gmail.com>
2024-05-13 21:43:11 -0400

    added li for how to play and added tailwind to h1 home page

--- client/src/components/landing/howtoplay.jsx
+++ client/src/components/landing/howtoplay.jsx
@@ -1,13 +1,17 @@
 const HowToPlay = () => {
   return (
     <>
-      <h2>How to Play</h2>
-      <p>
-        1. feed
-        2.sleep
-        3. every x amount of time your food, enegry and lseep with deplete
-        4. ...
-      </p>
+      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 my-8">
+        How to Play
+      </h2>
+
+      <ul>
+        <li>1.feed</li>
+        <li>2.sleep</li>
+        <li>
+          3.every x amount of time your food, enegry and sleep with deplete
+        </li>
+      </ul>
     </>
   );
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import About from '../components/landing/about';
 import HowToPlay from '../components/landing/howtoplay';
-import Nav from '../components/landing/nav';
 const Home = () => {
   return (
     <div className="container">
-      <h1>Welcome to Your Tomodachi</h1>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-7xl text-center my-8 bg-gradient-to-r from-violet-600 to-rose-400 text-transparent bg-clip-text">
+        Welcome to Your Tomodachi
+      </h1>
       <About />
       <HowToPlay />
-      <Nav />
     </div>
   );
 };
