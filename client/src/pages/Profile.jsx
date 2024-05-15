56db85874958b2702d18bb4b1ceb43010f5ea681
Lovertab <lovertabrown@gmail.com>
2024-05-15 17:31:16 -0400

    updated change username

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,3 +1,43 @@
-export default function Leaderboard() {
-  return <div>This is the Profile Page</div>;
-}
+import { Button } from '@/components/ui/button';
+import auth from '../utils/auth';
+import ChangeUsername from '../components/ChangeUsername';
+const Profile = () => {
+  return (
+    <>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-4xl text-center my-8 bg-gradient-to-r from-violet-600 to-rose-400 text-transparent bg-clip-text">
+        Welcome {auth.getProfile().data.username} to Your Portfolio
+      </h1>
+      <Button
+        variant="outline"
+        className="rounded-lg border drop-shadow-md hover:drop-shadow-xl dark:border-primary dark:border-3 dark:shadow-primary dark:shadow-md m-8"
+      >
+        Create a New Tomodachi
+      </Button>
+      <Button
+        variant="outline"
+        className="rounded-lg border drop-shadow-md hover:drop-shadow-xl dark:border-primary dark:border-3 dark:shadow-primary dark:shadow-md m-8"
+      >
+        Your Tomodachis
+        {/* preview of each emoji and there bars */}
+        {/* i want a Carousel */}
+      </Button>
+      <ChangeUsername
+        variant="outline"
+        className="rounded-lg border drop-shadow-md hover:drop-shadow-xl dark:border-primary dark:border-3 dark:shadow-primary dark:shadow-md m-8"
+      />
+      <Button
+        variant="outline"
+        className="rounded-lg border drop-shadow-md hover:drop-shadow-xl dark:border-primary dark:border-3 dark:shadow-primary dark:shadow-md m-8"
+      >
+        Delete a Game
+      </Button>
+    </>
+  );
+};
+export default Profile;
+
+// change username
+// delete a game
+// dashboard saying how long you've been with us
+
+//
