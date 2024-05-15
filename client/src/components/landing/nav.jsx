740516aac696d3c16f478189ed4075ec74b6a9ac
Nora Shala <norashala00@gmail.com>
2024-05-15 15:23:14 -0400

    nav bar changes

--- client/src/components/landing/nav.jsx
+++ client/src/components/landing/nav.jsx
@@ -6,36 +6,34 @@ export default function Nav() {
     <div>
       <main>
         {/* Position the nav bar absolutely with left, right, and top set to 0 */}
-        <nav className="absolute left-0 right-0 top-0 p-4 bg-white shadow-md w-full">
+        <nav className="absolute left-0 right-0 top-0 p-4 bg-foreground shadow-md w-full">
           {/* Use flex to align items and justify-between to space out the links and theme toggle */}
           <ul className="flex items-center justify-between w-full">
             {/* Group links in a flex container with horizontal spacing */}
-            <div className="flex space-x-4">
+            <div className="flex gap-4">
               <li>
-                <Link to="/landing" className="text-black">Home</Link>
+                <Link to="/landing" className="text-background">Home</Link>
               </li>
               <li>
-                <Link to="/profile" className="text-black">Profile</Link>
+                <Link to="/profile" className="text-background">Profile</Link>
               </li>
               <li>
-                <Link to="/leaderboard" className="text-black">Leaderboard</Link>
+                <Link to="/leaderboard" className="text-background">Leaderboard</Link>
               </li>
             </div>
             {/* Empty spacer to push Sign Up and Log In links to the right */}
             <div className="flex-grow" />
             {/* Place Sign Up and Log In links to the right side */}
-            <div className="flex space-x-4">
+            <div className="flex gap-4 items-center">
               <li className="ml-1">
-                <Link to="/signup" className="text-black">Sign up</Link>
+                <Link to="/signup" className="text-background">Sign up</Link>
               </li>
               <li className="ml-1">
-                <Link to="/login" className="text-black">Login</Link>
+                <Link to="/login" className="text-background">Login</Link>
               </li>
-            </div>
-            {/* Place ModeToggle to the right side */}
-            <div>
               <ModeToggle />
             </div>
+            {/* Place ModeToggle to the right side */}
           </ul>
         </nav>
       </main>
