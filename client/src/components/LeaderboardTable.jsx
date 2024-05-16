fca1c6a8f2d341e01dcadfb0f88fefb13d542a71
RonanCraft <SuperRonanCraft@gmail.com>
2024-05-16 02:02:43 -0400

    Leaderboard updated to new stats

--- client/src/components/LeaderboardTable.jsx
+++ client/src/components/LeaderboardTable.jsx
@@ -10,6 +10,7 @@ import {
 } from '@/components/ui/table';
 import { useQuery } from '@apollo/client';
 import { QUERY_ALL_USERS } from '../utils/queries';
+import { getTimeLeft } from '../lib/Game';
 
 export default function LeaderboardTable() {
   const { loading, data, error } = useQuery(QUERY_ALL_USERS);
@@ -23,49 +24,57 @@ export default function LeaderboardTable() {
 
   const sortedUsers = filteredUsers
     .map((user) => {
-      const total = user.gameData.reduce(
-        (acc, game) => {
-          return {
-            energy: acc.energy + game.energy,
-            food: acc.food + game.food,
-            happiness: acc.happiness + game.happiness,
-          };
-        },
-        { energy: 0, food: 0, happiness: 0 }
-      );
+      // const total = user.gameData.reduce(
+      //   (acc, game) => {
+      //     return {
+      //       energy: acc.energy + game.energy,
+      //       food: acc.food + game.food,
+      //       happiness: acc.happiness + game.happiness,
+      //     };
+      //   },
+      //   { energy: 0, food: 0, happiness: 0 }
+      // );
+      const largestTime = user.gameData.reduce((acc, game) => {
+        if (game.timeAlive > acc.timeAlive) return game;
+        return acc;
+      });
 
       return {
         ...user,
-        totalScore: total.energy + total.food + total.happiness,
+        totalScore: largestTime.timeAlive, //total.energy + total.food + total.happiness,
       };
     })
     .sort((a, b) => b.totalScore - a.totalScore);
 
   return (
-    <Table>
-      <TableCaption>
+    <>
+      <Table className="max-w-96 overflow-hidden mx-auto">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[50px]">Place</TableHead>
+            <TableHead>Username</TableHead>
+            <TableHead className="text-right">Highest Time</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {sortedUsers.map((user, index) => (
+            <TableRow key={user._id}>
+              <TableCell className="font-medium">{index + 1}</TableCell>
+              <TableCell>{user.username}</TableCell>
+              <TableCell className="text-right">
+                {getTimeLeft(user.totalScore)}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow></TableRow>
+        </TableFooter>
+      </Table>
+      <p className="text-center text-foreground/70">
         Don't see your name up there? Keep caring for your Tomodachis and you'll
         be up there in no time!
-      </TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[50px]">Place</TableHead>
-          <TableHead>Username</TableHead>
-          <TableHead className="text-right">Total Score</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {sortedUsers.map((user, index) => (
-          <TableRow key={user._id}>
-            <TableCell className="font-medium">{index + 1}</TableCell>
-            <TableCell>{user.username}</TableCell>
-            <TableCell className="text-right">{user.totalScore}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-      <TableFooter>
-        <TableRow></TableRow>
-      </TableFooter>
-    </Table>
+      </p>
+    </>
   );
 }
