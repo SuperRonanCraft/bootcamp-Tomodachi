5fded04b7d4410ba73c7a4bb78703edc78ccac80
RonanCraft <SuperRonanCraft@gmail.com>
2024-05-17 10:12:08 -0400

    profile page !loggedIn fix

--- client/src/pages/ProfileById.jsx
+++ client/src/pages/ProfileById.jsx
@@ -6,11 +6,12 @@ import auth from '../utils/auth';
 
 const ProfileUser = () => {
   const { userId } = useParams();
+
+  if (auth.loggedIn() && userId === auth.getProfile().data._id)
+    return <Navigate to={'/profile'} />;
   const { loading, data } = useQuery(QUERY_USER, {
     variables: { userId: userId },
   });
-  if (userId === auth.getProfile().data._id)
-    return <Navigate to={'/profile'} />;
   if (loading) return <h1>Loading...</h1>;
   return <ProfileDisplayer user={data.me} />;
 };
--- client/src/pages/ProfileDisplayer.jsx
+++ client/src/pages/ProfileDisplayer.jsx
@@ -7,12 +7,12 @@ const ProfileDisplayer = ({ user }) => {
     <div className="">
       <div className="text-center">
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-4xl text-center my-8 bg-gradient-to-r from-violet-600 to-rose-400 text-transparent bg-clip-text mt-32">
-          {auth.getProfile().data._id !== user._id
+          {!auth.loggedIn() || auth.getProfile().data._id !== user._id
             ? `Welcome to ${user.username}'s Profile`
             : `Welcome ${auth.getProfile().data.username} to Your Profile`}
         </h1>
         {/* Changing Username */}
-        {user._id === auth.getProfile().data._id && (
+        {auth.loggedIn() && user._id === auth.getProfile().data._id && (
           <ChangeUsername
             variant="outline"
             className="rounded-lg border drop-shadow-md hover:drop-shadow-xl dark:border-primary dark:border-3 dark:shadow-primary dark:shadow-md m-8"
